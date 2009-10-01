75cf34255411c49c9bc543fccd0ee1f544ef3965
David Cournapeau <cournape@gmail.com>
2009-10-01 22:41:51 +0900

    Add Commit object.

--- pygit/object.py
+++ pygit/object.py
@@ -9,7 +9,7 @@ def sort_by_values(d):
 # comparison when getting the type, but that may well be premature optimization
 # -- most of the code here should be done in cython anyway when speed is in a
 # issue.
-_TYPES_TO_ID = {'blob': 0, 'tree': 1}
+_TYPES_TO_ID = {'blob': 0, 'tree': 1, 'commit': 2}
 _ID_TO_TYPES = [i[0] for i in sort_by_values(_TYPES_TO_ID)]
 
 def parse_object(string):
@@ -68,6 +68,38 @@ def parse_tree(content):
         content = content[i+21:]
 
     return entries
+
+class CommitHeader:
+    def __init__(self, author, committer, tree, parents=None):
+        self.author = author
+        self.committer = committer
+        self.tree = tree
+        if parents:
+            self.parents = parents
+        else:
+            self.parents = []
+
+def _parse_commit_header(header):
+    author, committer, tree = None, None, None
+    parents = []
+    for line in header.splitlines():
+        if line.startswith('tree'):
+            tree = line.split(' ')[1].strip()
+        elif line.startswith('author'):
+            author = line.split(' ', 1)[1].strip()
+        elif line.startswith('committer'):
+            committer = line.split(' ', 1)[1].strip()
+        elif line.startswith('parent'):
+            parent = line.split(' ')[1].strip()
+            parents.append(parent)
+
+    if author is None or committer is None or tree is None:
+        raise ValueError("Error while parsing commit header.")
+    return CommitHeader(author, committer, tree, parents)
+
+def parse_commit(content):
+    header, message = content.split('\n\n', 1)
+    return _parse_commit_header(header), message
 
 def from_filename(file):
     content, tp = parse_object(open(file).read())
@@ -137,7 +169,29 @@ class Tree(GitObject):
     # def __str__(self):
     #     return "\n".join([e.pretty_str() for e in self.entries])
 
-_TYPE_TO_CLS = {'blob': Blob, 'tree': Tree}
+class Commit(GitObject):
+    _type = 'commit'
+    _type_id = _TYPES_TO_ID['commit']
+    def __init__(self, header, message, sort=True):
+        """If sort is True, sort parents."""
+        self.header = header
+        self.message = message
+        self.header.parents = sorted(header.parents)
+
+        self.content = self._compute_content()
+
+    def _compute_content(self):
+        cnt = []
+        cnt.append("tree %s" % self.header.tree)
+        for p in self.header.parents:
+            cnt.append("parent %s" % p)
+        cnt.append("author %s" % self.header.author)
+        cnt.append("committer %s" % self.header.committer)
+        cnt.append('')
+        cnt.append(self.message)
+        return "\n".join(cnt)
+
+_TYPE_TO_CLS = {'blob': Blob, 'tree': Tree, 'commit': Commit}
 
 if __name__ == '__main__':
     import os
