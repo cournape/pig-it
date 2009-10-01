4d9b2e8f26f4771ff661f7457463c3cbca1b1d53
David Cournapeau <cournape@gmail.com>
2009-10-01 20:09:57 +0900

    Add a second tree object with 2 file entries.

--- pygit/test.py
+++ pygit/test.py
@@ -7,13 +7,18 @@ from object import \
         from_filename, Blob, Tree, RawEntry
 
 SHA1_TO_FILE = {
-        '815fa52ea791bf9a0d152ca3386d61d3ad023a5a': 'tree',
+        '815fa52ea791bf9a0d152ca3386d61d3ad023a5a': 'tree1',
+        '00909da106de7af4a10f609de58136c47ca3221e': 'tree2',
+        'a94e2db7b97aab44f1ec897c10e0bdf2e5dbd80f': 'README',
         'dc1b915cba9cd6efd61c353fefb96823aaf2dd8f': 'TODO'}
 FILE_TO_SHA1 = dict([(v, k) for k, v in SHA1_TO_FILE.items()])
 
 # Raw content of each git object (as they are on-disk, excluding header)
 SHA1_TO_CONTENT = {
         '815fa52ea791bf9a0d152ca3386d61d3ad023a5a':
+            '%d %s\0%s' % (100644, 'TODO', a2b_hex(FILE_TO_SHA1['TODO'])),
+        '00909da106de7af4a10f609de58136c47ca3221e':
+            '%d %s\0%s' % (100644, 'README', a2b_hex(FILE_TO_SHA1['README'])) + \
             '%d %s\0%s' % (100644, 'TODO', a2b_hex(FILE_TO_SHA1['TODO'])),
         'dc1b915cba9cd6efd61c353fefb96823aaf2dd8f': 'TODO Content.\n'}
 
@@ -46,13 +51,12 @@ class TestTree:
         tree = Tree(raw_entries)
         assert tree.sha1() == sha1name
 
-    def test_parse(self):
+    def test_simple_parse(self):
         # file which contains the content of a tree object
-        filename = 'tree'
-        o = from_filename(filename)
+        for filename in ['tree1', 'tree2']:
+            o = from_filename(filename)
 
-        assert o.sha1() == FILE_TO_SHA1['tree']
+            assert o.sha1() == FILE_TO_SHA1[filename]
 
-        # Testing for sha1 should be enough, but we double check here
-        assert o.content == SHA1_TO_CONTENT[o.sha1()]
-
+            # Testing for sha1 should be enough, but we double check here
+            assert o.content == SHA1_TO_CONTENT[o.sha1()]
